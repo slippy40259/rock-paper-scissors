55a0c783e227dec5a3f327a85dbbf55260926cea
Slippy <andrewchung667@gmail.com>
2025-07-06 22:50:18 -0500

    Added a new function: getHumanChoice
    - Asks the user to choose rock, paper, or scissors
    - If the user choice is invalid, ask again until the choice is valid

--- game.js
+++ game.js
@@ -5,3 +5,14 @@ function getComputerChoice() {
         :  (choice==1) ? "paper"
         :  "scissors";
 }
+
+function getHumanChoice() {
+    let choice = prompt("Choose your weapon of choice: rock, paper, or scissors", "rock")
+    const hand = ["rock", "paper", "scissors"]
+
+    while (!hand.includes(choice)) {
+        choice = prompt("That weapon does not exist! Choose a different one: rock, paper, or scissors", "rock")
+    }
+    
+    return choice
+}
